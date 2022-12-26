d29381d7721284e37521abc2597887901d0bf982
Ben Green <bengreen@Ben-Green.local>
2022-12-26 11:30:30 +0000

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="intervals",
+    version="0.1",
+    url="http://github.com/benpgreen/intervals",
+    author="Ben Green",
+    license="Ben Green",
+    packages=["intervals"],
+)
