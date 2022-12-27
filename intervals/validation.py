a490c4e053576f9d8b8be50cbcfb2155cbc17db7
Ben Green <bengreen@Ben-Green.local>
2022-12-27 10:55:48 +0000

    Define initial validation functions.

--- intervals/validation.py
+++ intervals/validation.py
@@ -1,6 +1,18 @@
 def _validate_init_inputs(a, b, left_closed, right_closed):
-    pass
+    _check_is_number(a, b)
+    if not isinstance(left_closed, bool):
+        raise TypeError(f"left_closed={left_closed} should be a bool.")
+    if not isinstance(right_closed, bool):
+        raise TypeError(f"right_closed={right_closed} should be a bool.")
+    
+    # check Interval not empty
+    if a > b:
+        raise ValueError(f"a={a} must be <= b={b")
+    elif a == b and not (left_closed and right_closed):
+        raise ValueError("Interval should not be empty, if a=b then interval must be closed.")
 
 
 def _check_is_number(*numbers):
-    pass
+    for number in numbers:
+        if not isinstance(number, (float, int)):
+            raise TypeError(f"{number} should be an int or a float.")
